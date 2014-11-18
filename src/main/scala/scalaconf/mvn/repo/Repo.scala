c342935eb9bde76a687d5d9283c61e62b76b08fa
XuefengWu <benewu@gmail.com>
2014-11-18 17:50:32 +0800

    add repos

--- src/main/scala/scalaconf/mvn/repo/Repo.scala
+++ src/main/scala/scalaconf/mvn/repo/Repo.scala
@@ -8,7 +8,12 @@ object Repo {
     "http://repo1.maven.org/maven2/",
     "http://repository.springsource.com/ivy/bundles/external/",
     "http://repository.jboss.com/maven2/",
-    "https://repo.spring.io/libs-release/"
+    "https://repo.spring.io/libs-release/",
+    "https://repository.cloudera.com/cloudera/cloudera-repos/",
+    "https://repo.eclipse.org/content/repositories/paho-releases",
+    "https://repository.jboss.org/nexus/content/repositories/releases",
+    "https://repository.apache.org/content/repositories/releases",
+    "http://repository.mapr.com/maven"
   )
   
 }
