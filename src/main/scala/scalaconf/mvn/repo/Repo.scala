083da30c96a6d5a5cdd15015cb43fefa7e9f6b5d
XuefengWu <benewu@gmail.com>
2014-11-22 21:25:10 +0800

    add more Repository

--- src/main/scala/scalaconf/mvn/repo/Repo.scala
+++ src/main/scala/scalaconf/mvn/repo/Repo.scala
@@ -3,17 +3,22 @@ package scalaconf.mvn.repo
 object Repo {
 
   val resolvers = Seq(
-    "http://repo.typesafe.com/typesafe/releases/",
-    "http://repo.typesafe.com/typesafe/snapshots/",
-    "http://repo1.maven.org/maven2/",
-    "http://repository.springsource.com/ivy/bundles/external/",
-    "http://repository.jboss.com/maven2/",
-    "https://repo.spring.io/libs-release/",
-    "https://repository.cloudera.com/cloudera/cloudera-repos/",
+    "http://repo.typesafe.com/typesafe/releases",
+    "http://repo.typesafe.com/typesafe/snapshots",
+    "http://repo1.maven.org/maven2",
+    "http://repository.springsource.com/ivy/bundles/external",
+    "http://repository.jboss.com/maven2",
+    "https://repo.spring.io/libs-release",
+    "https://repository.cloudera.com/cloudera/cloudera-repos",
     "https://repo.eclipse.org/content/repositories/paho-releases",
     "https://repository.jboss.org/nexus/content/repositories/releases",
     "https://repository.apache.org/content/repositories/releases",
-    "http://repository.mapr.com/maven"
+    "http://repository.mapr.com/maven",
+    "http://mirrors.ibiblio.org/maven2",
+    "https://oss.sonatype.org/content/repositories/releases",
+    "https://oss.sonatype.org/content/repositories/staging",
+    "http://repo.springsource.org/plugins-release",
+    "http://repo.springsource.org/libs-release-remote"
   )
   
 }
