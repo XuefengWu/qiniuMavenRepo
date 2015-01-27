094e81fe6da666bddade1d9d808f570683028d84
Xuefeng Wu <xfwu@thoughtworks.com>
2015-01-27 22:40:19 +0800

    use http client instand of async http client

--- src/main/scala/scalaconf/mvn/repo/handler/ArtifactFetcher.scala
+++ src/main/scala/scalaconf/mvn/repo/handler/ArtifactFetcher.scala
@@ -9,6 +9,8 @@ import com.qiniu.api.auth.digest.Mac
 import com.qiniu.api.resumableio.ResumeableIoApi
 import com.qiniu.api.rs.PutPolicy
 import com.typesafe.config.ConfigFactory
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.DefaultHttpClient
 
 import scalaconf.mvn.repo.store
 
@@ -27,19 +29,55 @@ object ArtifactFetcher {
 
 }
 
-case class ArtifactUri(resolvers: Seq[String], path: String, clientOpt : Option[AsyncHttpClient] = None)
+case class ArtifactUri(resolvers: Seq[String], path: String, clientOpt: Option[AsyncHttpClient] = None)
 
 class ArtifactFetcher(p: PutPolicy, mac: Mac) extends Actor with ActorLogging {
 
 
   override def receive: Receive = {
-    case ArtifactUri(resolvers, path, clientOpt) if resolvers.nonEmpty =>
-      if(store.FetchStore.get(path).isEmpty) {
-        fetch(resolvers, path, resolvers.head + path, clientOpt)
+    case ArtifactUri(resolvers, path, _) if resolvers.nonEmpty =>
+      if (store.FetchStore.get(path).isEmpty) {
+        fetch(resolvers, path, resolvers.head + path)
       }
   }
 
-  private def fetch(resolvers: Seq[String], path: String, url: String, clientOpt : Option[AsyncHttpClient]): Unit = {
+
+  private def fetch(resolvers: Seq[String], path: String, url: String): Unit = {
+    log.debug(s"fetch $url")
+    val httpclient = new DefaultHttpClient()
+    val httpgets = new HttpGet(url)
+    val response = httpclient.execute(httpgets)
+
+    if (response != null) {
+      val status = response.getStatusLine.getStatusCode
+      if (status < 400) {
+        val relocation = Option(response.getFirstHeader("Location")).map(_.getValue)
+        if (relocation.isEmpty) {
+          val entity = response.getEntity()
+          ResumeableIoApi.put(entity.getContent, p.token(mac), path.tail)
+        } else {
+          relocation.foreach(ref => fetch(resolvers, path, url))
+        }
+      } else {
+        tryFetchForNext(resolvers, path, url)
+      }
+
+    } else {
+      tryFetchForNext(resolvers, path, url)
+    }
+
+
+  }
+
+  private def tryFetchForNext(resolvers: Seq[String], path: String, url: String): Unit = {
+    if (resolvers.nonEmpty) {
+      fetch(resolvers.tail, path, url)
+    } else {
+      store.FetchStore.put(path, store.FetchResult.Fail)
+    }
+  }
+
+  private def fetch(resolvers: Seq[String], path: String, url: String, clientOpt: Option[AsyncHttpClient]): Unit = {
 
     val client = clientOpt.getOrElse(new AsyncHttpClient())
     store.FetchStore.put(path, store.FetchResult.InProgress)
@@ -50,12 +88,12 @@ class ArtifactFetcher(p: PutPolicy, mac: Mac) extends Actor with ActorLogging {
       override def onThrowable(t: Throwable): Unit = {
 
         store.FetchStore.del(path)
-        tryFetchForNext(resolvers, path, client)
+        tryFetchForNext(resolvers, path, url, client)
       }
 
       override def onCompleted(): Unit = {
         val data = bytes.toByteArray
-        if(data.size > 0){
+        if (data.size > 0) {
           log.debug(s"=========put data size: ${data.size}=============${path.tail}========")
           ResumeableIoApi.put(new ByteArrayInputStream(data), p.token(mac), path.tail)
           store.FetchStore.put(path, store.FetchResult.Ok)
@@ -72,7 +110,7 @@ class ArtifactFetcher(p: PutPolicy, mac: Mac) extends Actor with ActorLogging {
         val statusCode = responseStatus.getStatusCode()
         if (statusCode >= 400) {
           store.FetchStore.del(path)
-          tryFetchForNext(resolvers, path, client)
+          tryFetchForNext(resolvers, path, url, client)
           STATE.ABORT
         } else {
           STATE.CONTINUE
@@ -87,15 +125,16 @@ class ArtifactFetcher(p: PutPolicy, mac: Mac) extends Actor with ActorLogging {
         })
       }
     })
-    if(f.isDone){
+    if (f.isDone) {
       client.close()
     }
 
   }
 
-  private def tryFetchForNext(resolvers: Seq[String], path: String, client: AsyncHttpClient): Unit = {
-    if(resolvers.nonEmpty) {
-      self ! ArtifactUri(resolvers.tail, path)
+  private def tryFetchForNext(resolvers: Seq[String], path: String, url: String, client: AsyncHttpClient): Unit = {
+    if (resolvers.nonEmpty) {
+      fetch(resolvers.tail, path, url, Some(client))
+      //self ! ArtifactUri(resolvers.tail, path)
     } else {
       store.FetchStore.put(path, store.FetchResult.Fail)
       client.close()
